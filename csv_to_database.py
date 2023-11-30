d45d4e7ea91709ab07dbb5189cf53a4499b1f28c
felixwalberg <fwalberg@uvm.edu>
2023-11-30 14:23:17 -0500

    Added people and user data, script to add them to database

--- csv_to_database.py
+++ csv_to_database.py
@@ -9,6 +9,8 @@ cur = conn.cursor()
 
 # Make sure to start with fresh tables
 cur.execute("DROP TABLE IF EXISTS courses;")
+cur.execute("DROP TABLE IF EXISTS users;")
+cur.execute("DROP TABLE IF EXISTS people;")
 # TODO: REPLACE TABLES WITH RELEVANT NAMES
 # cur.execute("DROP TABLE IF EXISTS posts;")
 # cur.execute("DROP TABLE IF EXISTS users;")
@@ -30,6 +32,41 @@ for row in contents:
 file.close()
 
 # TODO: UPDATE FOR RELEVANT TABLES 
+# create a table for users csv
+cur.execute('''CREATE TABLE users(netid TEXT, password TEXT)''')
+
+# for users data, we used randomly generated names and passwords from these sources:
+# https://www.behindthename.com/random/random.php?gender=both&number=2&sets=1&surname=&norare=yes&usage_eng=1
+# https://www.dinopass.com/
+# open file and add contents into table
+file = open('users.csv')
+contents = csv.reader(file)
+headers = next(contents)
+
+insert_records = '''INSERT INTO users('netid','password') VALUES(?,?)'''
+for row in contents:
+    if row != []:
+        cur.execute(insert_records, row)
+# close the file
+file.close()
+
+# create a table for people csv
+cur.execute('''CREATE TABLE people(firstName TEXT, lastName TEXT, netid TEXT, hold TEXT, type TEXT)''')
+
+# for people data, we used randomly generated names from this source:
+# https://www.behindthename.com/random/random.php?gender=both&number=2&sets=1&surname=&norare=yes&usage_eng=1
+# open file and add contents into table
+file = open('people.csv')
+contents = csv.reader(file)
+headers = next(contents)
+
+insert_records = '''INSERT INTO people('firstName','lastName','netid','hold','type') VALUES(?,?,?,?,?)'''
+for row in contents:
+    if row != []:
+        cur.execute(insert_records, row)
+# close the file
+file.close()
+
 # #create a table for posts csv
 # cur.execute('''CREATE TABLE posts(user_id INTEGER, post TEXT, posted_date TEXT)''')
 
