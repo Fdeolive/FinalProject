898a3345e48b2d8c1a97eb225b8a45027c5b5bc5
felixwalberg <fwalberg@uvm.edu>
2023-12-05 21:30:29 -0500

    Changed spelling on button route in admin course info

--- Flask.py
+++ Flask.py
@@ -264,7 +264,7 @@ def adminREMOVE(CRN):
         return 'There was a problem deleting that course'
 
 ##Info On courses
-@app.route("/adminCourseINFO/<CRN>")
+@app.route("/adminCourseINFO/<CRN>", methods=['GET','POST'])
 def adminCourseINFO(CRN):
     ###Figure out how to display on the screen
     if (request.method=="POST"):
@@ -274,7 +274,7 @@ def adminCourseINFO(CRN):
             answers= db.session.query(sum(enrollment.netid)).filter(enrollment.CRN==CRN)
         else:
             answers= courses.query.filter.func.max(people.GPA).join(enrollment, enrollment.netid == people.netid, isouter=False).filter(enrollment.CRN==CRN).scalar()
-        return render_template("adminCourseINFOANSWER.html",answers=answers)
+        return render_template("adminCourseINFOAnswer.html",answers=answers)
    
     else:  
         classes= courses.query.get_or_404(CRN)
